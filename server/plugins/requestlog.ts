f36ba4b518f616c505e00c94bf4288531973f126
Michael Karbowiak <michael@karbowiak.dk>
2025-01-05 22:48:14 +0100

    Use the forwarded for IP

--- server/plugins/requestlog.ts
+++ server/plugins/requestlog.ts
@@ -3,11 +3,7 @@ import { nitroApp } from "nitropack/runtime/internal/app";
 export default defineNitroPlugin(() => {
 
     nitroApp.hooks.hook('request', (event) => {
-        let requestIp = event.context.clientAddress ||
-            event.node.req.connection.remoteAddress ||
-            event.node.req.socket.remoteAddress ||
-            getRequestIP(event) ||
-            getRequestIP(event, { xForwardedFor: true });
+        let requestIp = getRequestIP(event, { xForwardedFor: true });
 
         // Output request log info similar to nginx
         console.log(`[${new Date().toISOString()} "${event.node.req.method} ${event.node.req.url} HTTP/${event.node.req.httpVersion}" ${event.node.req.headers['user-agent']} ${requestIp}`);
