28a68ff8c54beb6753e2b72c89543f31a16c6368
Michael Karbowiak <michael@karbowiak.dk>
2025-01-05 22:58:33 +0100

    Clean out logging since the correct header has been found

--- server/plugins/requestlog.ts
+++ server/plugins/requestlog.ts
@@ -5,12 +5,6 @@ export default defineNitroPlugin(() => {
     nitroApp.hooks.hook('request', (event) => {
         let requestIp = getRequestIP(event, { xForwardedFor: true });
 
-        console.log(event.context.clientAddress,
-            event.node.req.connection.remoteAddress,
-            event.node.req.socket.remoteAddress,
-            getRequestIP(event),
-            getRequestIP(event, { xForwardedFor: true }));
-
         // Output request log info similar to nginx
         console.log(`[${new Date().toISOString()} "${event.node.req.method} ${event.node.req.url} HTTP/${event.node.req.httpVersion}" ${event.node.req.headers['user-agent']} ${requestIp}`);
     });
