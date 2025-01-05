d7df7f0311624ed980df8d5503b417719365e2ec
Michael Karbowiak <michael@karbowiak.dk>
2025-01-05 20:10:37 +0100

    Add pagination to the characters api for listing all the character ids, and output 100k IDs pr. page

--- server/api/characters/index.get.ts
+++ server/api/characters/index.get.ts
@@ -2,7 +2,10 @@ import { defineEventHandler } from 'h3';
 import { ICharacter } from '../../interfaces/ICharacter';
 
 export default defineEventHandler(async (event) => {
-    let characters: ICharacter[] = await Characters.find({}, { character_id: 1 });
+    let query = getQuery(event);
+    let page = query?.page ? parseInt(query.page as string) : 1;
+    let characters: ICharacter[] = await Characters.find({}, { character_id: 1 }, { limit: 100000, skip: (page - 1) * 100000 });
+
     // Return a single array containing all the IDs
     return characters.map((character) => character.character_id);
 });
