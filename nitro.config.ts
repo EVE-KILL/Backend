835447d0fc0f566c70d6834ebbb8ccf717688f86
Michael Karbowiak <michael@karbowiak.dk>
2025-01-11 16:50:23 +0100

    Add a task that queues up unprocessed killmails

--- nitro.config.ts
+++ nitro.config.ts
@@ -115,6 +115,7 @@ export default defineNitroConfig({
     ],
     '*/5 * * * *': [
       'findNewCharacters',
+      'processUnprocessedKillmails',
     ],
     '* * * * *': [
       'tqStatus',
---
+++ server/tasks/processUnprocessedKillmails.ts
@@ -0,0 +1,40 @@
+import { addKillmail } from "~/queue/Killmail";
+
+export default defineTask({
+  meta: {
+    name: "processUnprocessedKillmails",
+    description: "Process Killmails that needs processing",
+  },
+  async run({ payload, context }) {
+    // Limit it to 10k mails at a time
+    let limit = 10000;
+
+    let unprocessedKillmails = await KillmailsESI.aggregate([
+      {
+        $lookup: {
+          from: "Killmails",
+          localField: "killmail_id",
+          foreignField: "killmail_id",
+          as: "matched"
+        }
+      },
+      {
+        $match: { matched: { $eq: [] } }
+      },
+      { $project: { _id: 0, killmail_id: 1, killmail_hash: 1 } },
+      { $limit: limit }
+    ]);
+
+    let foundKillmailCount = 0;
+    for (let killmail of unprocessedKillmails) {
+      foundKillmailCount++;
+      addKillmail(killmail.killmail_id, killmail.killmail_hash as string, 0);
+    }
+
+    return {
+      result: {
+        foundKillmailCount
+      }
+    }
+  }
+});
