5cd9c35f1c5c48e353d236df2ceb05a79a9d96ad
Michael Karbowiak <michael@karbowiak.dk>
2025-02-18 11:14:21 +0100

    Add members api endpoints for corp and alli

---
+++ server/api/corporations/[id]/members.ts
@@ -0,0 +1,16 @@
+import { defineEventHandler } from 'h3';
+
+export default defineEventHandler(async (event) => {
+    const corporationId: number | null = event.context.params?.id ? parseInt(event.context.params.id) : null;
+    if (!corporationId) {
+        return { error: "Corporation ID not provided" };
+    }
+
+    // Find all members that are in this corporation
+    let members = await Characters.find({ corporation_id: corporationId }, { _id: 0, character_id: 1, name: 1 });
+    if (members.length === 0) {
+        return { error: "No members found" };
+    }
+
+    return members;
+});
