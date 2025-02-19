59976bc784e096d6353ead2a86628597e5163c2d
Michael Karbowiak <michael@karbowiak.dk>
2025-02-19 10:25:18 +0100

    Add top for character

---
+++ server/api/characters/[id]/top.ts
@@ -0,0 +1,25 @@
+import { defineEventHandler } from 'h3';
+import { topShips, topSystems } from '~/helpers/TopLists';
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const characterId: number | null = event.context.params?.id ? parseInt(event.context.params.id) : null;
+    const topType = query.type as string || '';
+    if (!characterId) {
+        return { error: "Character ID not provided" };
+    }
+    if (topType === '') {
+        return { error: "Type not provided" };
+    }
+
+    switch (topType) {
+        case 'ships':
+            return await topShips('character_id', characterId, 7, 10);
+
+        case 'systems':
+            return await topSystems('character_id', characterId, 7, 10);
+
+        case 'regions':
+            return await topSystems('character_id', characterId, 7, 10);
+    };
+});
