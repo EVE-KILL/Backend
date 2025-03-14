da1ffbae65e012fff34a487687a66623d9788be0
Michael Karbowiak <michael@karbowiak.dk>
2025-03-14 16:06:45 +0100

    Return 0 instead of null

--- server/api/status/index.get.ts
+++ server/api/status/index.get.ts
@@ -408,6 +408,6 @@ export default defineEventHandler(async () => {
 });
 
 function formatNumber(num: number): number {
-  return Number(num.toLocaleString("da-DK")); // Lets be real, US using commas as decimal separator is just wrong.
+  return Number(num.toLocaleString("da-DK")) || 0; // Lets be real, US using commas as decimal separator is just wrong.
   // When are ya'll gonne give up and use metric? plebs..
 }
